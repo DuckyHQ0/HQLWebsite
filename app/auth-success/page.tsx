d206aea36e6abe187660738cf5e7d099db2a7147
dukc <dukkcc@proton.me>
2024-01-28 14:41:24 -0800

    added more text

--- app/auth-success/page.tsx
+++ app/auth-success/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-col p-64 gap-12 justify-center items-start h-[700px] max-w-[1200px] w-full bg-bg-1 rounded-out border border-stroke-1">
       <h1 className="text-1 font-bold">Success!</h1>
-      <p className="text-body">Your account has been verified.</p>
+      <p className="text-body">Your account has been verified. You can close this tab now.</p>
     </div>
   );
 }
